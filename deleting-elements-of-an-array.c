8651e53269f532fb065d74a152698ef2d75d01b8
Isha Negi <itsishanegi@gmail.com>
2024-10-13 17:12:04 +0530

    Add files via upload

---
+++ deleting-elements-of-an-array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{ int arr[5];
+printf("Enter 5 numbers: ");
+for(int i=0;i<5;i++){
+    scanf("%d",&arr[i]);
+}
+
+for(int j=0;j<5;j++){
+   if(j==2||j==4){
+       continue;
+   }
+   printf("%d ",arr[j]);
+}
+}
